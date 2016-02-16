5b4e411c299ff0b6cbd529af4903022785379a93
Gerardo Teruel <gerardogtn@gmail.com>
2016-02-16 07:25:54 -0600

    :rocket: Add simple example with library linking

---
+++ cplusplus/01MotionDataLogger/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "IEmoStateDLL.h"
+#include "Iedk.h"
+#include "IedkErrorCode.h"
+
+
+//g++ main.cpp -I/home/inspirecave/Documents/tallerEmotiv/include/
+int main(int argc, char const *argv[]) {
+  std::cout << "Prueba" << std::endl;
+  return 0;
+}
